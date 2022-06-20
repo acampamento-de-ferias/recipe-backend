1d05488e61a2c40a36f4466a1242bb82ab131171
Gabriel Viegas <gabrielgl13@hotmail.com>
2022-06-20 19:25:00 -0300

    add docker

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
-    port: 5432,
+    port: 3306,
     username: "admin",
     password: "admin",
     database: "recipes"
--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,8 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
 app.listen(4000, () => console.log("Server is Running"))
