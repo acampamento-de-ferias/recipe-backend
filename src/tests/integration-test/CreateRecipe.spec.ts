a435e5b6d4843d434e20da7ae675ed98c717a1e6
Gabriel Viegas <gabrielgl13@hotmail.com>
2022-07-18 22:02:00 -0300

    test husky

--- src/tests/integration-test/CreateRecipe.spec.ts
+++ src/tests/integration-test/CreateRecipe.spec.ts
@@ -54,7 +54,7 @@ describe('Create Recipe', () => {
       .attach('file', 'src/tests/images/bolo-de-laranja.jpg');
 
     expect(response.statusCode).toEqual(201);
-    expect(response.body).toHaveProperty('idfff');
+    expect(response.body).toHaveProperty('iddd');
   });
 
   it('Should be able to create a new recipe without image', async () => {
